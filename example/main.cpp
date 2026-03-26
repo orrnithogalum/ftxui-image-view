1a73b0ec5cd476836cd01bf97824845e530c9092
Leora <syl20.row@gmail.com>
2026-03-26 08:18:26 +0100

    Revert back to original example

--- example/main.cpp
+++ example/main.cpp
@@ -31,62 +31,30 @@ int main(int argc, char **argv)
 
     auto cell = [](const std::string& path){ return ftxui::image_view(path) | ftxui::border | ftxui::size(ftxui::WIDTH, ftxui::EQUAL, 30) | ftxui::size(ftxui::HEIGHT, ftxui::EQUAL, 30); };
 
-    int displayIndexLeft{0};
-    int displayIndexRight{1};
-    int displayIndexLeft1{2};
-    int displayIndexRight1{3};
-    auto leftDisplay = ftxui::Renderer([&]{
-        return ftxui::vbox(
-            cell(pictures[displayIndexLeft]),
-            cell(pictures[displayIndexLeft1])
-        );
-    });
-
-    auto rightDisplay = ftxui::Renderer([&]{
-        return ftxui::vbox(
-            cell(pictures[displayIndexRight]),
-            cell(pictures[displayIndexRight1])
-        );
+    int displayIndex{0};
+    auto catDisplay = ftxui::Renderer([&]{
+        return cell(pictures[displayIndex]);
     });
 
     auto selectButton = ftxui::Button("Click", [&]{
         std::random_device seed;
         std::default_random_engine en(seed());
         std::uniform_int_distribution<> dist(0, pictures.size() - 1);
-
-        displayIndexLeft = dist(en);
-        displayIndexRight = dist(en);
-
-        displayIndexLeft1 = dist(en);
-        displayIndexRight1 = dist(en);
+        displayIndex = dist(en);
     });
 
     auto mainPannelContainer = ftxui::Container::Vertical({
-        leftDisplay,
-        rightDisplay,
+        catDisplay,
         selectButton
     });
 
     auto screen = ftxui::ScreenInteractive::FitComponent();
 
     auto mainPannelRender = ftxui::Renderer(mainPannelContainer, [&]{
-        static auto last_frame_time = std::chrono::steady_clock::now();
-        auto now = std::chrono::steady_clock::now();
-
-        auto frame_time = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_frame_time).count();
-        last_frame_time = now;
-
-        screen.PostEvent(ftxui::Event::Custom);
-
         return ftxui::vbox({
-            ftxui::hbox({
-                leftDisplay->Render() | ftxui::size(ftxui::WIDTH, ftxui::LESS_THAN, 60) | ftxui::size(ftxui::HEIGHT, ftxui::EQUAL, 30),
-                ftxui::separator(),
-                rightDisplay->Render() | ftxui::size(ftxui::WIDTH, ftxui::LESS_THAN, 60) | ftxui::size(ftxui::HEIGHT, ftxui::EQUAL, 30),
-            }),
+            catDisplay->Render() | ftxui::size(ftxui::WIDTH, ftxui::LESS_THAN, 60),
             ftxui::separator(),
-            ftxui::text("Frame Time: " + std::to_string(frame_time) + " ms"),
-            selectButton->Render(),
+            selectButton->Render() | ftxui::size(ftxui::HEIGHT, ftxui::GREATER_THAN, 10)
         });
     });
 
